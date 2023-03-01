4d9fd7949595e2ee50fbd3d95bed093ad8a86971
Lulú <93228729+LuCoronel@users.noreply.github.com>
2023-03-01 14:59:38 -0300

    deshago cambios

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <p> Hola </p>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
